792d5a50c4563659ddd2bcb4b66042ee0dcd592d
Gabe Quinn <gabe.quinn@icloud.com>
2019-09-26 10:03:28 -0400

    V2.0 updated js game fully functional, next version will update css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,12 @@
-
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h","i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var wins = 0;
 var losses = 0;
 var remaining = 9;
+var guessed = [];
 
+
+var directionsText = document.getElementById("directions-text");
 var thinkingText = document.getElementById("thinking-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
@@ -14,23 +16,28 @@ var guessedText = document.getElementById("guessed-text");
 document.onkeyup = function(event) {
    var userGuess = event.key.toLowerCase();
    var computerGuess = letters[Math.floor(Math.random() * letters.length)];
+  
+
 
    if (userGuess === computerGuess) {
        wins++;
+       remaining = 9;
    }
-   else {
+   else if (userGuess !== computerGuess) {
        remaining--;
+       guessed.push(userGuess);
+       guessedText.textContent = guessed.join(", ");
    }
 
    if (remaining === 0) {
        losses++;
+       remaining = 9;
    }
-   
-   
+   directionsText.textContent = " "
    winsText.textContent = "wins: " + wins;
    lossesText.textContent = "losses: " + losses;
-   guessesText.textContent = "Remaining Guesses Left: " + remaining;
+   guessesText.textContent = "Remaining Guesses: " + remaining;
+};
    
 
-};
 
